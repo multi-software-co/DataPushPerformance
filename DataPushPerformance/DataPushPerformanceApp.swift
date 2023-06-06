cc085b7f94f7576c661bc618fad95022b684f8d7
Fernando Barbat <fernando@remotion.com>
2023-06-06 14:15:06 -0300

    Initial Commit

---
+++ DataPushPerformance/DataPushPerformanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataPushPerformanceApp.swift
+//  DataPushPerformance
+//
+//  Created by Fernando Barbat on 6/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DataPushPerformanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
